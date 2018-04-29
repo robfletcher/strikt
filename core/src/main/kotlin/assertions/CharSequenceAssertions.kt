66cdfd9c476974e85fac84f200545278d6038667
Rob Fletcher <rfletcher@netflix.com>
2018-04-29 08:00:01 -0700

    braces

--- core/src/main/kotlin/assertions/CharSequenceAssertions.kt
+++ core/src/main/kotlin/assertions/CharSequenceAssertions.kt
@@ -1,13 +1,22 @@
 package assertions
 
 fun <T : CharSequence> Assertion<T>.hasLength(expected: Int): Assertion<T> =
-  if (expected == target.length) this
-  else throw AssertionError("Expected length of $target to be $expected but was ${target.length}")
+  if (expected == target.length) {
+    this
+  } else {
+    throw AssertionError("Expected length of $target to be $expected but was ${target.length}")
+  }
 
 fun <T : CharSequence> Assertion<T>.isLowerCase(): Assertion<T> =
-  if (target.all { it.isLowerCase() }) this
-  else throw AssertionError("Expected $target to be lower case")
+  if (target.all { it.isLowerCase() }) {
+    this
+  } else {
+    throw AssertionError("Expected $target to be lower case")
+  }
 
 fun <T : CharSequence> Assertion<T>.startsWith(expected: Char): Assertion<T> =
-  if (target.startsWith(expected)) this
-  else throw AssertionError("Expected $target to start with $expected")
+  if (target.startsWith(expected)) {
+    this
+  } else {
+    throw AssertionError("Expected $target to start with $expected")
+  }
--- core/src/main/kotlin/assertions/CollectionAssertions.kt
+++ core/src/main/kotlin/assertions/CollectionAssertions.kt
@@ -1,10 +1,13 @@
 package assertions
 
 fun <T : Collection<E>, E> Assertion<T>.hasSize(expected: Int): Assertion<T> =
-  if (expected == target.size) this
-  else throw AssertionError("Expected size of $target to be $expected but was ${target.size}")
+  if (expected == target.size) {
+    this
+  } else {
+    throw AssertionError("Expected size of $target to be $expected but was ${target.size}")
+  }
 
-fun <T : Collection<E>, E> Assertion<T>.allMatch(predicate: Assertion<E>.() -> Unit) =
+fun <T : Iterable<E>, E> Assertion<T>.allMatch(predicate: Assertion<E>.() -> Unit) =
   target.forEach {
     Assertion(it).predicate()
   }
