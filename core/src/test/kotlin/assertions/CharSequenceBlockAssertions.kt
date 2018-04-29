99f1b794bfcde8458f2620958fc58b3d78cc9177
Rob Fletcher <rfletcher@netflix.com>
2018-04-29 00:47:58 -0700

    separate files

--- core/src/test/kotlin/assertions/CharSequenceBlockAssertions.kt
+++ core/src/test/kotlin/assertions/CharSequenceBlockAssertions.kt
@@ -3,17 +3,21 @@ package assertions
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.*
 
-internal object CharSequenceInlineAssertions : Spek({
+internal object CharSequenceBlockAssertions : Spek({
 
   describe("hasLength assertion") {
     it("passes if the target has the expected length") {
       shouldPass {
-        expect("covfefe").hasLength(7)
+        expect("covfefe") {
+          hasLength(7)
+        }
       }
     }
     it("fails if the target does not have the expected length") {
       shouldFail {
-        expect("covfefe").hasLength(1)
+        expect("covfefe") {
+          hasLength(1)
+        }
       }
     }
   }
