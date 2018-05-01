f6084c741b39e86e4cf2eeee2e27c6db1211c469
Rob Fletcher <rfletcher@netflix.com>
2018-05-01 09:06:51 -0700

    private classes with hideous names instead of anonymous inner classes

--- core/src/main/kotlin/assertions/Assertion.kt
+++ core/src/main/kotlin/assertions/Assertion.kt
@@ -24,51 +24,53 @@ internal class ReportingAssertion<T>(
   private val subject: T
 ) : Assertion<T> {
   override fun evaluate(description: String, predicate: AssertionContext.(T) -> Unit) {
-    object : AssertionContext {
-      override fun success() {
-        reporter.report(result(Status.Success, description, subject))
-      }
-
-      override fun failure() {
-        reporter.report(result(Status.Failure, description, subject))
-      }
-    }
-      .predicate(subject)
+    AssertionContextImpl(reporter, description, subject).predicate(subject)
   }
 
   override fun evaluateNested(description: String, predicate: NestedAssertionContext.(T) -> Unit) {
-    object : NestedAssertionContext {
-      private val aggregatingReporter = AggregatingReporter()
-
-      override fun success() {
-        reporter.report(result(Status.Success, description, subject, aggregatingReporter.results))
-      }
-
-      override fun failure() {
-        reporter.report(result(Status.Failure, description, subject, aggregatingReporter.results))
-      }
-
-      override fun <T> expect(subject: T): Assertion<T> {
-        return ReportingAssertion(aggregatingReporter, subject)
-      }
-
-      override fun <T> expect(subject: T, block: Assertion<T>.() -> Unit): Assertion<T> {
-        return ReportingAssertion(aggregatingReporter, subject)
-          .apply(block)
-      }
-
-      override val anyFailed: Boolean
-        get() = aggregatingReporter.anyFailed
-
-      override val allFailed: Boolean
-        get() = aggregatingReporter.allFailed
-
-      override val anySucceeded: Boolean
-        get() = aggregatingReporter.anySucceeded
-
-      override val allSucceeded: Boolean
-        get() = aggregatingReporter.allSucceeded
-    }
-      .predicate(subject)
+    NestedAssertionContextImpl(reporter, description, subject).predicate(subject)
   }
+}
+
+private class AssertionContextImpl<T>(val reporter: Reporter, val description: String, val subject: T) : AssertionContext {
+  override fun success() {
+    reporter.report(result(Status.Success, description, subject))
+  }
+
+  override fun failure() {
+    reporter.report(result(Status.Failure, description, subject))
+  }
+}
+
+private class NestedAssertionContextImpl<T>(val reporter: Reporter, val description: String, val subject: T) : NestedAssertionContext {
+  private val aggregatingReporter = AggregatingReporter()
+
+  override fun success() {
+    reporter.report(result(Status.Success, description, subject, aggregatingReporter.results))
+  }
+
+  override fun failure() {
+    reporter.report(result(Status.Failure, description, subject, aggregatingReporter.results))
+  }
+
+  override fun <T> expect(subject: T): Assertion<T> {
+    return ReportingAssertion(aggregatingReporter, subject)
+  }
+
+  override fun <T> expect(subject: T, block: Assertion<T>.() -> Unit): Assertion<T> {
+    return ReportingAssertion(aggregatingReporter, subject)
+      .apply(block)
+  }
+
+  override val anyFailed: Boolean
+    get() = aggregatingReporter.anyFailed
+
+  override val allFailed: Boolean
+    get() = aggregatingReporter.allFailed
+
+  override val anySucceeded: Boolean
+    get() = aggregatingReporter.anySucceeded
+
+  override val allSucceeded: Boolean
+    get() = aggregatingReporter.allSucceeded
 }
