e470951082d6fe8cc8b65de937a7dc9fac3c42f6
Rob Fletcher <rfletcher@netflix.com>
2018-05-16 14:19:43 -0700

    Added a few new char sequence assertions
    Issue #29

--- strikt-core/src/main/kotlin/strikt/assertions/CharSequence.kt
+++ strikt-core/src/main/kotlin/strikt/assertions/CharSequence.kt
@@ -1,6 +1,7 @@
 package strikt.assertions
 
 import strikt.api.Assertion
+import kotlin.text.RegexOption.IGNORE_CASE
 
 /**
  * Asserts that the subject has a [CharSequence.length] of exactly [expected].
@@ -47,12 +48,51 @@ fun <T : CharSequence> Assertion<T>.matches(expected: Regex): Assertion<T> =
   }
 
 /**
+ * Asserts that the subject is a full match for the [expected] regular
+ * expression regardless of case.
+ */
+fun <T : CharSequence> Assertion<T>.matchesIgnoringCase(expected: Regex): Assertion<T> =
+  passesIf("matches the regular expression /${expected.pattern}/ (ignoring case)") {
+    Regex(expected.pattern, IGNORE_CASE).let {
+      matches(it)
+    }
+  }
+
+/**
  * Asserts that the subject contains a match for the [expected] regular
  * expression.
  */
 fun <T : CharSequence> Assertion<T>.contains(expected: Regex): Assertion<T> =
-  passesIf("matches the regular expression /${expected.pattern}/") {
+  passesIf("contains a match for the regular expression /${expected.pattern}/") {
     contains(expected)
+  }
+
+/**
+ * Asserts that the subject contains a match for the [expected] regular
+ * expression regardless of case.
+ */
+fun <T : CharSequence> Assertion<T>.containsIgnoringCase(expected: Regex): Assertion<T> =
+  passesIf("contains a match for the regular expression /${expected.pattern}/ (ignoring case)") {
+    Regex(expected.pattern, IGNORE_CASE).let {
+      contains(it)
+    }
+  }
+
+/**
+ * Asserts that the subject contains the [expected] substring.
+ */
+fun <T : CharSequence> Assertion<T>.contains(expected: CharSequence): Assertion<T> =
+  passesIf("contains \"$expected\"") {
+    contains(expected)
+  }
+
+/**
+ * Asserts that the subject contains the [expected] substring regardless of
+ * case.
+ */
+fun <T : CharSequence> Assertion<T>.containsIgnoringCase(expected: CharSequence): Assertion<T> =
+  passesIf("contains \"$expected\" (ignoring case)") {
+    contains(expected, ignoreCase = true)
   }
 
 /**
