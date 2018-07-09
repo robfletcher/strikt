4a626f8a0365a38047c9988a44bc1004dac86d6e
Rob Fletcher <rfletcher@netflix.com>
2018-07-09 14:11:39 -0700

    Upgrade to Spek 2

--- strikt-core/src/test/kotlin/strikt/Throws.kt
+++ strikt-core/src/test/kotlin/strikt/Throws.kt
@@ -1,9 +1,8 @@
 package strikt
 
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
 import strikt.api.expect
 import strikt.api.throws
 import strikt.assertions.isA
