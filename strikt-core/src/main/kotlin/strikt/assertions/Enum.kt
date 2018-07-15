d3e99bf4255342d4ffa08133e826458496c5bf4c
Rob Fletcher <rfletcher@netflix.com>
2018-07-15 13:03:16 -0700

    Fix lint error

--- strikt-core/src/main/kotlin/strikt/assertions/Enum.kt
+++ strikt-core/src/main/kotlin/strikt/assertions/Enum.kt
@@ -2,8 +2,8 @@ package strikt.assertions
 
 import strikt.api.Assertion
 
-val <T: Enum<T>> Assertion<T>.name: Assertion<String>
+val <T : Enum<T>> Assertion<T>.name: Assertion<String>
   get() = map(Enum<T>::name)
 
-val <T: Enum<T>> Assertion<T>.ordinal: Assertion<Int>
+val <T : Enum<T>> Assertion<T>.ordinal: Assertion<Int>
   get() = map(Enum<T>::ordinal)
