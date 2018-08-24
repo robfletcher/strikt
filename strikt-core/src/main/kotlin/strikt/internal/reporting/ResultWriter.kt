a62a6a0927b2ac19d88d44cc26a42a7b4208906c
Rob Fletcher <rfletcher@netflix.com>
2018-08-24 13:10:19 -0700

    atomic assertions' message just has the path to that one assertion

--- strikt-core/src/main/kotlin/strikt/internal/reporting/ResultWriter.kt
+++ strikt-core/src/main/kotlin/strikt/internal/reporting/ResultWriter.kt
@@ -1,18 +1,29 @@
 package strikt.internal.reporting
 
 import strikt.internal.AssertionNode
+import strikt.internal.AssertionResult
 import java.io.StringWriter
 
 internal interface ResultWriter {
   fun writeTo(writer: Appendable, node: AssertionNode<*>)
+
   fun writeTo(writer: Appendable, results: Iterable<AssertionNode<*>>) =
     results.forEach { writeTo(writer, it) }
+
+  fun writePathTo(writer: Appendable, node: AssertionResult<*>)
 }
 
 internal fun AssertionNode<*>.writeToString(resultWriter: ResultWriter = DefaultResultWriter()) =
   StringWriter()
     .use { writer ->
       resultWriter.writeTo(writer, this)
+      writer.toString()
+    }
+
+internal fun AssertionResult<*>.writePartialToString(resultWriter: ResultWriter = DefaultResultWriter()) =
+  StringWriter()
+    .use { writer ->
+      resultWriter.writePathTo(writer, this)
       writer.toString()
     }
 
