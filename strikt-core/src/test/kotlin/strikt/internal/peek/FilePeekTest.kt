f9b4d35ad1f13bb8fba8be026fe3dbd7b7113c1e
Christoph Sturm <me@christophsturm.com>
2018-09-11 10:47:37 +0200

    move test back because the filtering is now configurable

--- strikt-core/src/test/kotlin/strikt/internal/peek/FilePeekTest.kt
+++ strikt-core/src/test/kotlin/strikt/internal/peek/FilePeekTest.kt
@@ -1,44 +1,41 @@
-package strikt.test
+package strikt.internal.peek
 
 import org.junit.jupiter.api.Test
 import strikt.api.expectThat
 import strikt.assertions.endsWith
 import strikt.assertions.isEqualTo
-import strikt.internal.peek.FileInfo
-import strikt.internal.peek.FilePeek
 
-/*
- * i moved this file to a different package to avoid the package filtering
- */
 class FilePeekTest {
+  val fileName = "src/test/kotlin/strikt/internal/peek/FilePeekTest.kt"
+
   @Test
   fun `can get FileInfo`() {
-    val fileInfo = FilePeek.getCallerFileInfo()
+    val fileInfo = FilePeek.getCallerFileInfo(filterMethod("can get"))
 
     expectThat(fileInfo) {
       map(FileInfo::sourceFileName)
-        .endsWith("src/test/kotlin/strikt/test/FilePeekTest.kt")
+        .endsWith(fileName)
       map(FileInfo::line)
-        .isEqualTo("val fileInfo = FilePeek.getCallerFileInfo()")
+        .isEqualTo("""val fileInfo = FilePeek.getCallerFileInfo(filterMethod("can get"))""")
     }
   }
 
   @Test
   fun `can get FileInfo for a block`() {
-    val fileInfo = { FilePeek.getCallerFileInfo() }()
+    val fileInfo = { FilePeek.getCallerFileInfo(filterMethod("can get")) }()
 
     expectThat(fileInfo) {
       map(FileInfo::sourceFileName)
-        .endsWith("src/test/kotlin/strikt/test/FilePeekTest.kt")
+        .endsWith(fileName)
       map(FileInfo::line)
-        .isEqualTo("val fileInfo = { FilePeek.getCallerFileInfo() }()")
+        .isEqualTo("""val fileInfo = { FilePeek.getCallerFileInfo(filterMethod("can get")) }()""")
     }
   }
 
   @Test
   fun `can get block body even when it contains multiple `() {
     fun mapMethod(@Suppress("UNUSED_PARAMETER") block: () -> Unit) =
-      FilePeek.getCallerFileInfo { it.methodName.startsWith("can get") }
+      FilePeek.getCallerFileInfo(filterMethod("can get"))
 
     val fileInfo = mapMethod {
       /* LOL! I'm a block body*/
@@ -50,9 +47,14 @@ class FilePeekTest {
   }
 }
 
+fun filterMethod(methodName: String): (StackTraceElement) -> Boolean =
+  { it.methodName.startsWith(methodName) }
+
 class FilePeekTestWithDifferentNameThanItsFile {
   @Test
   fun `finds classes that have a different name than the file they are in`() {
-    FilePeek.getCallerFileInfo()
+    expectThat(FilePeek.getCallerFileInfo(filterMethod("finds")))
+      .map { it.line }
+      .isEqualTo("expectThat(FilePeek.getCallerFileInfo(filterMethod(\"finds\")))")
   }
 }
