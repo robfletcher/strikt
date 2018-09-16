1c986596cf57d60a15ca76782a00de3945886aa8
Christoph Sturm <me@christophsturm.com>
2018-09-16 19:04:57 +0200

    document that subexpectations can be described.
    actually i thought that this did not work, but then i wrote a test and it did

--- strikt-core/src/test/kotlin/strikt/Nested.kt
+++ strikt-core/src/test/kotlin/strikt/Nested.kt
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import strikt.api.expect
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
 import strikt.assertions.isLowerCase
 import strikt.assertions.isNull
 import strikt.assertions.isUpperCase
+import strikt.assertions.message
 
 @DisplayName("assertions in blocks")
 internal class Nested {
@@ -27,5 +30,21 @@ internal class Nested {
         "  ✗ is lower case"
       assertEquals(expected, error.message)
     }
+  }
+
+  @Test
+  fun `nested expectations can be described`() {
+    expectThat(fails {
+      expect {
+        (1 until 3).forEach {
+          that("foo").describedAs("pass $it").isEqualTo("bar")
+        }
+      }
+    }).message.isEqualTo(
+      """▼ Expect that pass 1:
+  ✗ is equal to "bar" : found "foo"
+▼ Expect that pass 2:
+  ✗ is equal to "bar" : found "foo""""
+    )
   }
 }
