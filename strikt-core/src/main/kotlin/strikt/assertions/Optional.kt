f35858c644c51d78ebe85fb529be674328ecccdc
Rob Fletcher <rfletcher@netflix.com>
2019-02-24 08:25:00 -0800

    Assertions that act on Optional

---
+++ strikt-core/src/main/kotlin/strikt/assertions/Optional.kt
@@ -0,0 +1,26 @@
+package strikt.assertions
+
+import strikt.api.Assertion
+import java.util.Optional
+
+/**
+ * Asserts that an `Optional` contains a value (is not empty) and returns an
+ * assertion builder whose subject is the value.
+ */
+fun <T> Assertion.Builder<Optional<T>>.isPresent(): Assertion.Builder<T> =
+  assertThat("a value is present", Optional<T>::isPresent)
+    .get { get() }
+
+/**
+ * Asserts that an `Optional` does not contain a value.
+ */
+fun <T> Assertion.Builder<Optional<T>>.isAbsent(): Assertion.Builder<Optional<T>> =
+  assertThat("no value is present") {
+    !it.isPresent
+  }
+
+/**
+ * Maps an assertion on a Java `Optional` to a Kotlin nullable type.
+ */
+fun <T> Assertion.Builder<Optional<T>>.toNullable(): Assertion.Builder<T?> =
+  get { orElse(null) }
---
+++ strikt-core/src/test/kotlin/strikt/assertions/OptionalAssertions.kt
@@ -0,0 +1,69 @@
+package strikt.assertions
+
+import dev.minutest.junit.JUnit5Minutests
+import dev.minutest.rootContext
+import org.junit.jupiter.api.assertThrows
+import org.opentest4j.AssertionFailedError
+import strikt.api.Assertion
+import strikt.api.expectThat
+import java.util.Optional
+
+internal object OptionalAssertions : JUnit5Minutests {
+  fun isPresentTests() = rootContext<Assertion.Builder<Optional<String>>> {
+    context("the optional subject contains a value") {
+      fixture { expectThat(Optional.of("fnord")) }
+
+      test("the assertion passes") {
+        isPresent()
+      }
+    }
+
+    context("the optional subject is empty") {
+      fixture { expectThat(Optional.empty()) }
+
+      test("the assertion passes") {
+        assertThrows<AssertionFailedError> {
+          isPresent()
+        }
+      }
+    }
+  }
+
+  fun isAbsentTests() = rootContext<Assertion.Builder<Optional<String>>> {
+    context("the optional subject contains a value") {
+      fixture { expectThat(Optional.of("fnord")) }
+
+      test("the assertion passes") {
+        assertThrows<AssertionFailedError> {
+          isAbsent()
+        }
+      }
+    }
+
+    context("the optional subject is empty") {
+      fixture { expectThat(Optional.empty()) }
+
+      test("the assertion passes") {
+        isAbsent()
+      }
+    }
+  }
+
+  fun toNullableTests() = rootContext<Assertion.Builder<Optional<String>>> {
+    context("the optional subject contains a value") {
+      fixture { expectThat(Optional.of("fnord")) }
+
+      test("the subject is transformed to the optional value") {
+        toNullable().isNotNull().isEqualTo("fnord")
+      }
+    }
+
+    context("the optional subject is empty") {
+      fixture { expectThat(Optional.empty()) }
+
+      test("the subject is transformed to null") {
+        toNullable().isNull()
+      }
+    }
+  }
+}
