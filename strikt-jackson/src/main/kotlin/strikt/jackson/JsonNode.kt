5f7a4a7af3c08d5ab4646edf5c605674e97f5c12
Rob Fletcher <rfletcher@netflix.com>
2019-03-03 08:29:21 -0800

    Enhancements to Jackson module

--- strikt-jackson/src/main/kotlin/strikt/jackson/JsonNode.kt
+++ strikt-jackson/src/main/kotlin/strikt/jackson/JsonNode.kt
@@ -6,9 +6,11 @@ import com.fasterxml.jackson.databind.node.BooleanNode
 import com.fasterxml.jackson.databind.node.JsonNodeType
 import com.fasterxml.jackson.databind.node.JsonNodeType.ARRAY
 import com.fasterxml.jackson.databind.node.JsonNodeType.BOOLEAN
+import com.fasterxml.jackson.databind.node.JsonNodeType.MISSING
 import com.fasterxml.jackson.databind.node.JsonNodeType.NUMBER
 import com.fasterxml.jackson.databind.node.JsonNodeType.OBJECT
 import com.fasterxml.jackson.databind.node.JsonNodeType.STRING
+import com.fasterxml.jackson.databind.node.MissingNode
 import com.fasterxml.jackson.databind.node.NumericNode
 import com.fasterxml.jackson.databind.node.ObjectNode
 import com.fasterxml.jackson.databind.node.TextNode
@@ -30,7 +32,9 @@ fun <T : JsonNode> Assertion.Builder<T>.has(fieldName: String): Assertion.Builde
  * Maps the subject node of the assertion builder to the node accessed by
  * [fieldName].
  *
- * @return an assertion builder whose subject is the named field.
+ * @return an assertion builder whose subject is the named field. If the path is
+ * invalid the subject of the returned assertion builder will be a
+ * [com.fasterxml.jackson.databind.node.MissingNode].
  */
 fun <T : JsonNode> Assertion.Builder<T>.path(fieldName: String): Assertion.Builder<JsonNode> =
   get { path(fieldName) }
@@ -41,8 +45,9 @@ fun <T : JsonNode> Assertion.Builder<T>.path(fieldName: String): Assertion.Build
  * @return an assertion builder over the same subject that is now known to be
  * an [ObjectNode].
  */
+@Suppress("UNCHECKED_CAST")
 fun <T : JsonNode> Assertion.Builder<T>.isObject(): Assertion.Builder<ObjectNode> =
-  assertNodeTypeIs(OBJECT)
+  hasNodeType(OBJECT) as Assertion.Builder<ObjectNode>
 
 /**
  * Asserts that the subject node is a JSON array.
@@ -50,8 +55,9 @@ fun <T : JsonNode> Assertion.Builder<T>.isObject(): Assertion.Builder<ObjectNode
  * @return an assertion builder over the same subject that is now known to be
  * an [ArrayNode].
  */
+@Suppress("UNCHECKED_CAST")
 fun <T : JsonNode> Assertion.Builder<T>.isArray(): Assertion.Builder<ArrayNode> =
-  assertNodeTypeIs(ARRAY)
+  hasNodeType(ARRAY) as Assertion.Builder<ArrayNode>
 
 /**
  * Asserts that the subject node is a JSON text node.
@@ -59,8 +65,9 @@ fun <T : JsonNode> Assertion.Builder<T>.isArray(): Assertion.Builder<ArrayNode> 
  * @return an assertion builder over the same subject that is now known to be
  * a [TextNode].
  */
+@Suppress("UNCHECKED_CAST")
 fun <T : JsonNode> Assertion.Builder<T>.isTextual(): Assertion.Builder<TextNode> =
-  assertNodeTypeIs(STRING)
+  hasNodeType(STRING) as Assertion.Builder<TextNode>
 
 /**
  * Asserts that the subject node is a JSON numeric node.
@@ -68,8 +75,9 @@ fun <T : JsonNode> Assertion.Builder<T>.isTextual(): Assertion.Builder<TextNode>
  * @return an assertion builder over the same subject that is now known to be
  * a [NumericNode].
  */
+@Suppress("UNCHECKED_CAST")
 fun <T : JsonNode> Assertion.Builder<T>.isNumber(): Assertion.Builder<NumericNode> =
-  assertNodeTypeIs(NUMBER)
+  hasNodeType(NUMBER) as Assertion.Builder<NumericNode>
 
 /**
  * Asserts that the subject node is a JSON boolean node.
@@ -77,16 +85,72 @@ fun <T : JsonNode> Assertion.Builder<T>.isNumber(): Assertion.Builder<NumericNod
  * @return an assertion builder over the same subject that is now known to be
  * a [BooleanNode].
  */
-fun <T : JsonNode> Assertion.Builder<T>.isBoolean(): Assertion.Builder<BooleanNode> =
-  assertNodeTypeIs(BOOLEAN)
-
 @Suppress("UNCHECKED_CAST")
-private fun <T : JsonNode, R : JsonNode> Assertion.Builder<T>.assertNodeTypeIs(
-  type: JsonNodeType
-): Assertion.Builder<R> =
-  assert("is a $type node") {
+fun <T : JsonNode> Assertion.Builder<T>.isBoolean(): Assertion.Builder<BooleanNode> =
+  hasNodeType(BOOLEAN) as Assertion.Builder<BooleanNode>
+
+/**
+ * Asserts that the subject node is a JSON missing node.
+ *
+ * @return an assertion builder over the same subject that is now known to be
+ * a [MissingNode].
+ */
+@Suppress("UNCHECKED_CAST")
+fun <T : JsonNode> Assertion.Builder<T>.isMissing(): Assertion.Builder<MissingNode> =
+  hasNodeType(MISSING) as Assertion.Builder<MissingNode>
+
+/**
+ * Maps the assertion to an assertion on the subject node's text value.
+ *
+ * Be aware that if the node is not a text node this will map to an assertion on
+ * `null`.
+ *
+ * @see isTextual
+ */
+fun <T : JsonNode> Assertion.Builder<T>.textValue(): Assertion.Builder<String?> =
+  get { textValue() }
+
+/**
+ * Maps the assertion to an assertion on the subject node's number value.
+ *
+ * Be aware that if the node is not a numeric node this will map to an assertion
+ * on `null`.
+ *
+ * @see isNumber
+ */
+fun <T : JsonNode> Assertion.Builder<T>.numberValue(): Assertion.Builder<Number?> =
+  get { numberValue() }
+
+/**
+ * Maps the assertion to an assertion on the subject node's boolean value.
+ *
+ * Be aware that if the node is not a boolean node this will map to an assertion
+ * on `false`.
+ *
+ * @see isBoolean
+ */
+fun <T : JsonNode> Assertion.Builder<T>.booleanValue(): Assertion.Builder<Boolean> =
+  get { booleanValue() }
+
+/**
+ * Asserts that the subject node's [JsonNode.getNodeType] method returns
+ * [nodeType].
+ *
+ * Convenient aliases for this assertion exist for common node types that also
+ * narrow the subject type of the resulting assertion builder.
+ *
+ * @see isObject
+ * @see isArray
+ * @see isTextual
+ * @see isNumber
+ * @see isBoolean
+ */
+fun <T : JsonNode> Assertion.Builder<T>.hasNodeType(
+  nodeType: JsonNodeType
+): Assertion.Builder<T> =
+  assert("is a $nodeType node") {
     when (it.nodeType) {
-      type -> pass()
+      nodeType -> pass()
       else -> fail(actual = it.nodeType, description = "found a %s node")
     }
-  } as Assertion.Builder<R>
+  }
--- strikt-jackson/src/test/kotlin/strikt/jackson/JsonNodeAssertions.kt
+++ strikt-jackson/src/test/kotlin/strikt/jackson/JsonNodeAssertions.kt
@@ -2,13 +2,18 @@ package strikt.jackson
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.node.JsonNodeType.MISSING
+import com.fasterxml.jackson.databind.node.JsonNodeType.NULL
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import dev.minutest.junit.JUnit5Minutests
 import dev.minutest.rootContext
 import org.junit.jupiter.api.assertThrows
 import org.opentest4j.AssertionFailedError
 import strikt.api.expectThat
+import strikt.assertions.isA
 import strikt.assertions.isEqualTo
+import strikt.assertions.isFalse
+import strikt.assertions.isNull
+import strikt.assertions.isTrue
 
 internal object JsonNodeAssertions : JUnit5Minutests {
 
@@ -23,7 +28,8 @@ internal object JsonNodeAssertions : JUnit5Minutests {
             "Protector of Mexico"
           ],
           "year-of-birth": 1818,
-          "deceased": true
+          "deceased": true,
+          "place-of-birth": null
         }
       """
       )
@@ -129,6 +135,85 @@ internal object JsonNodeAssertions : JUnit5Minutests {
           expectThat(this)
             .isBoolean()
         }
+      }
+    }
+
+    context("isMissing assertion") {
+      test("passes if the node is a missing node") {
+        expectThat(this)
+          .path("date-of-birth")
+          .isMissing()
+      }
+
+      test("fails if the node is not a missing node") {
+        assertThrows<AssertionFailedError> {
+          expectThat(this)
+            .isMissing()
+        }
+      }
+    }
+
+    context("hasNodeType assertion") {
+      test("passes if the node is the specified type") {
+        expectThat(this)
+          .path("place-of-birth")
+          .hasNodeType(NULL)
+      }
+
+      test("fails if the node is a different type") {
+        assertThrows<AssertionFailedError> {
+          expectThat(this)
+            .hasNodeType(NULL)
+        }
+      }
+    }
+
+    context("textValue mapping") {
+      test("maps a text node to its text value") {
+        expectThat(this)
+          .path("name")
+          .textValue()
+          .isEqualTo("Joshua Abraham Norton")
+      }
+
+      test("maps any other node type to null") {
+        expectThat(this)
+          .path("titles")
+          .textValue()
+          .isNull()
+      }
+    }
+
+    context("numberValue mapping") {
+      test("maps a numeric node to its number value") {
+        expectThat(this)
+          .path("year-of-birth")
+          .numberValue()
+          .isEqualTo(1818)
+          .isA<Int>()
+      }
+
+      test("maps any other node type to null") {
+        expectThat(this)
+          .path("name")
+          .numberValue()
+          .isNull()
+      }
+    }
+
+    context("booleanValue mapping") {
+      test("maps a boolean node to its boolean value") {
+        expectThat(this)
+          .path("deceased")
+          .booleanValue()
+          .isTrue()
+      }
+
+      test("maps any other node type to false") {
+        expectThat(this)
+          .path("name")
+          .booleanValue()
+          .isFalse()
       }
     }
   }
