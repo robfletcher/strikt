c2470d6bfdf368533cd4cc8492f6e1fe90ab6fb3
Rob Fletcher <rfletcher@netflix.com>
2019-03-15 20:15:09 -0700

    - `Assertion.Builder<ServletResponse>.contentTypeIsCompatibleWith` assertion

---
+++ strikt-spring/src/main/kotlin/strikt/spring/ServletResponse.kt
@@ -0,0 +1,25 @@
+package strikt.spring
+
+import org.springframework.http.MediaType
+import strikt.api.Assertion
+import javax.servlet.ServletResponse
+
+/**
+ * Maps this assertion to an assertion on the content type of the servlet
+ * response. If the subject has no `Content-Type` header the subject of the
+ * mapped assertion is `null`.
+ */
+val <T : ServletResponse> Assertion.Builder<T>.contentType: Assertion.Builder<MediaType?>
+  get() = get { contentType?.let(MediaType::parseMediaType) }
+
+/**
+ * Asserts that the content type of the subject is compatible with [expected].
+ *
+ * @see MediaType.isCompatibleWith
+ */
+fun <T : ServletResponse> Assertion.Builder<T>.contentTypeIsCompatibleWith(
+  expected: MediaType
+): Assertion.Builder<T> =
+  assertThat("content type is compatible with $expected") {
+    it.contentType.let(MediaType::parseMediaType).isCompatibleWith(expected)
+  }
--- strikt-spring/src/test/kotlin/strikt/spring/ServletResponseAssertions.kt
+++ strikt-spring/src/test/kotlin/strikt/spring/ServletResponseAssertions.kt
@@ -2,23 +2,28 @@ package strikt.spring
 
 import dev.minutest.junit.JUnit5Minutests
 import dev.minutest.rootContext
+import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
+import org.opentest4j.AssertionFailedError
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK
 import org.springframework.http.MediaType
 import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.http.MediaType.APPLICATION_JSON_UTF8
+import org.springframework.http.MediaType.IMAGE_PNG
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import strikt.api.Assertion
 import strikt.api.expectThat
 import strikt.assertions.isA
 import strikt.assertions.isNotNull
 import strikt.spring.app.App
 import strikt.spring.app.Controller
+import javax.servlet.ServletResponse
 
 @ExtendWith(SpringExtension::class)
 @SpringBootTest(
@@ -26,26 +31,43 @@ import strikt.spring.app.Controller
   webEnvironment = MOCK
 )
 @AutoConfigureMockMvc
-internal class MockHttpServletResponseAssertions : JUnit5Minutests {
+internal class ServletResponseAssertions : JUnit5Minutests {
   @Autowired
   lateinit var mvc: MockMvc
 
-  fun tests() = rootContext<MvcResult> {
-    context("a request for XML") {
+  fun tests() = rootContext<Assertion.Builder<ServletResponse>> {
+    context("a request for JSON") {
       fixture {
         mvc
           .perform(get("/").accept(APPLICATION_JSON))
           .andExpect(status().isOk)
           .andReturn()
+          .response
+          .run { expectThat(this) }
       }
 
-      context("content type mapping") {
+      context("contentType mapping") {
         test("maps to the response content type") {
-          expectThat(response)
-            .contentType
+          contentType
             .isNotNull()
             .isA<MediaType>()
             .isCompatibleWith(APPLICATION_JSON)
+        }
+      }
+
+      context("contentTypeIsCompatibleWith assertion") {
+        test("passes if the content type is an exact match") {
+          contentTypeIsCompatibleWith(APPLICATION_JSON_UTF8)
+        }
+
+        test("passes if the content type is a sub-type") {
+          contentTypeIsCompatibleWith(APPLICATION_JSON)
+        }
+
+        test("fails if the content type is not compatible") {
+          assertThrows<AssertionFailedError> {
+            contentTypeIsCompatibleWith(IMAGE_PNG)
+          }
         }
       }
     }
