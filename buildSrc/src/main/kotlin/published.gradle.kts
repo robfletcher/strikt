8efdb432ac3dd6dab3d518eee7f9f7a88f38db84
Mike Kobit <mkobit@gmail.com>
2019-04-04 15:26:09 -0500

    Use accessor generation for precompiled scripts (added in Gradle 5.3)

--- buildSrc/src/main/kotlin/published.gradle.kts
+++ buildSrc/src/main/kotlin/published.gradle.kts
@@ -1,5 +1,4 @@
 import com.jfrog.bintray.gradle.BintrayExtension
-import org.jetbrains.dokka.gradle.DokkaTask
 
 plugins {
   id("org.jetbrains.dokka")
@@ -13,7 +12,7 @@ plugins.withId("kotlin") {
     enabled = false
   }
 
-  val dokka by tasks.getting(DokkaTask::class) {
+  tasks.dokka  {
     outputFormat = "html"
     outputDirectory = "$buildDir/javadoc"
     jdkVersion = 9
@@ -23,7 +22,7 @@ plugins.withId("kotlin") {
     group = "build"
     description = "Assembles Javadoc jar from Dokka API docs"
     archiveClassifier.set("javadoc")
-    from(tasks.named("dokka"))
+    from(tasks.dokka)
   }
 
   publishing {
@@ -64,7 +63,7 @@ publishing {
   }
 }
 
-configure<BintrayExtension> {
+bintray {
   user = System.getenv("BINTRAY_USER")
   key = System.getenv("BINTRAY_KEY")
   pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
