0f67da172146831fd5beebf92fdaffef47282816
Casey Brooks <cjbrooks12@gmail.com>
2019-06-17 10:47:07 -0500

    Updates Orchid breaking changes from 0.17 update

--- site/site.gradle.kts
+++ site/site.gradle.kts
@@ -18,7 +18,7 @@ Commands:
 
 plugins {
   id("nebula.kotlin")
-  id("com.eden.orchidPlugin") version "0.17.1"
+  id("com.eden.orchidPlugin") version "0.17.4"
 }
 
 repositories {
@@ -27,10 +27,11 @@ repositories {
 }
 
 dependencies {
-  orchidCompile("io.github.javaeden.orchid:OrchidCore:0.17.1")
-  orchidRuntime("io.github.javaeden.orchid:OrchidDocs:0.17.1")
-  orchidRuntime("io.github.javaeden.orchid:OrchidPluginDocs:0.17.1")
-  orchidRuntime("io.github.javaeden.orchid:OrchidKotlindoc:0.17.1")
+  orchidCompile("io.github.javaeden.orchid:OrchidCore:0.17.4")
+  orchidRuntime("io.github.javaeden.orchid:OrchidDocs:0.17.4")
+  orchidRuntime("io.github.javaeden.orchid:OrchidPluginDocs:0.17.4")
+  orchidRuntime("io.github.javaeden.orchid:OrchidKotlindoc:0.17.4")
+  orchidRuntime("io.github.javaeden.orchid:OrchidGithub:0.17.4")
 }
 
 project.version = "${project.version}"
