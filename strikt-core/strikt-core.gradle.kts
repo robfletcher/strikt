bcee438ba5d38708a27c973ab5f04c0dae20b6f4
Christoph Sturm <me@christophsturm.com>
2019-07-18 18:53:33 +0200

    update to newer version of filepeek with a less confusing class name

--- strikt-core/strikt-core.gradle.kts
+++ strikt-core/strikt-core.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.2")
   implementation("org.opentest4j:opentest4j:1.2.0")
-  implementation("com.christophsturm:filepeek:0.1.0")
+  implementation("com.christophsturm:filepeek:0.1.1")
 
   testImplementation("dev.minutest:minutest:1.7.0")
 }
