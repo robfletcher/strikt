30bac06e7d5966489b06b639735296ff2110f4b6
Rob Fletcher <robfletcher@users.noreply.github.com>
2019-09-25 06:36:34 -0700

    Fixed opentest4j transitive dependency
    Fixes #180

--- strikt-core/strikt-core.gradle.kts
+++ strikt-core/strikt-core.gradle.kts
@@ -6,9 +6,10 @@ plugins {
 }
 
 dependencies {
+  api("org.opentest4j:opentest4j:1.2.0")
+  
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
-  implementation("org.opentest4j:opentest4j:1.2.0")
   implementation("com.christophsturm:filepeek:0.1.1")
 
   testImplementation("dev.minutest:minutest:1.7.0")
