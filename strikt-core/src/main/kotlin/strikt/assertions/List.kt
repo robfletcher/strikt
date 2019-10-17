27536c4627203d8e5249c370cca5728517ed983b
Segun Famisa <segunfamisa@gmail.com>
2019-10-17 20:06:34 +0200

    Add assertions for sequence of values being present in a list

--- strikt-core/src/main/kotlin/strikt/assertions/List.kt
+++ strikt-core/src/main/kotlin/strikt/assertions/List.kt
@@ -1,6 +1,7 @@
 package strikt.assertions
 
 import strikt.api.Assertion.Builder
+import java.util.*
 
 /**
  * Maps this assertion to an assertion on the element at index [i] in the
@@ -17,3 +18,27 @@ operator fun <T : List<E>, E> Builder<T>.get(range: IntRange): Builder<List<E>> 
   get("elements [$range] %s") {
     subList(range.first, range.last + 1)
   }
+
+/**
+ * Asserts that all [elements] are present in the subject in exactly the same order
+ */
+fun <T : List<E>, E> Builder<T>.containsSequence(vararg elements: E) =
+  containsSequence(elements.toList())
+
+/**
+ * Asserts that all [elements] are present in the subject in exactly the same order
+ */
+fun <T : List<E>, E> Builder<T>.containsSequence(elements: List<E>) =
+  assert("contains the sequence: %s in exactly the same order", elements.toList()) { subject ->
+    val subjectList = subject.toList()
+    val expectedList = elements.toList()
+
+    when {
+      subjectList.isEmpty() -> fail("subject cannot be empty")
+      expectedList.isEmpty() -> fail("expected sequence cannot empty")
+      subjectList.size < expectedList.size -> fail("expected sequence cannot be longer than subject")
+      Collections.indexOfSubList(subjectList, expectedList) != -1 -> pass()
+      else -> fail()
+    }
+  }
+
