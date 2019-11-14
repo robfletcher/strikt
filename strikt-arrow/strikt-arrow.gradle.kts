7e377903da7f91400c35d5cc0757bd70294a19e7
Thanh Le <lenguyenthanh@hotmail.com>
2019-11-14 10:50:23 +0100

    Bump Arrow version 0.10.3

--- strikt-arrow/strikt-arrow.gradle.kts
+++ strikt-arrow/strikt-arrow.gradle.kts
@@ -10,8 +10,8 @@ plugins {
 dependencies {
   api(project(":strikt-core"))
 
-  compileOnly("io.arrow-kt:arrow-core:0.10.0")
-  testImplementation("io.arrow-kt:arrow-core:0.10.0")
+  compileOnly("io.arrow-kt:arrow-core:0.10.3")
+  testImplementation("io.arrow-kt:arrow-core:0.10.3")
 
   testImplementation("dev.minutest:minutest:1.7.0")
 }
