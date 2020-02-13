5d97ab0dd25f6a76424108d10d80163e34186359
Christoph Sturm <me@christophsturm.com>
2020-02-13 15:35:40 +0100

    update filepeek to fix running on windows

--- strikt-core/strikt-core.gradle.kts
+++ strikt-core/strikt-core.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 dependencies {
   api("org.opentest4j:opentest4j:1.2.0")
-  api("com.christophsturm:filepeek:0.1.1")
+  api("com.christophsturm:filepeek:0.1.2")
 
   implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
