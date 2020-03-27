aa34b4937c11aa5919fafe6e8535e204501f8c26
Rob Fletcher <rfletcher@netflix.com>
2020-03-27 06:57:18 -0700

    Kotlin, JUnit & Spring updates

--- strikt-spring/strikt-spring.gradle.kts
+++ strikt-spring/strikt-spring.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
 
   api(project(":strikt-core"))
 
-  implementation(platform("org.springframework.boot:spring-boot-dependencies:2.2.5.RELEASE"))
+  implementation(platform("org.springframework.boot:spring-boot-dependencies:2.2.6.RELEASE"))
   compileOnly("org.springframework:spring-test")
   compileOnly("org.springframework:spring-web")
   compileOnly("javax.servlet:javax.servlet-api")
