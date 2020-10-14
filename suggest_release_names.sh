19fde82a80b68510a8ad349a53a65e361c70787f
Rob Fletcher <rfletcher@netflix.com>
2020-10-14 06:30:37 -0700

    shellcheck release name script

--- suggest_release_names.sh
+++ suggest_release_names.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env sh
-function require {
-  command -v $1 >/dev/null 2>&1 || {
+#!/usr/bin/env bash
+require() {
+  command -v "$1" >/dev/null 2>&1 || {
     echo >&2 "Script requires $1 but it's not available."
     exit 1
   }
@@ -14,9 +14,9 @@ then
   exit 1
 fi
 
-PREVIOUS=`curl 'https://api.github.com/repos/robfletcher/strikt/releases?per_page=1000' -H "Authorization: token $GITHUB_TOKEN" -s | jq '.[] | .name' | tr -d "\"" | sort`
-NAMES=`curl 'http://www.voidstate.com/name_generator/index.php?action=display_results' -s --data 'action=display_results&dd1%5B%5D=All_Adjective&dd1%5B%5D=All_Noun&numberToDisplay=10' | pup 'table td strong text{}' | sort`
-while read name
+PREVIOUS=$(curl 'https://api.github.com/repos/robfletcher/strikt/releases?per_page=1000' -H "Authorization: token $GITHUB_TOKEN" -s | jq '.[] | .name' | tr -d "\"" | sort)
+NAMES=$(curl 'http://www.voidstate.com/name_generator/index.php?action=display_results' -s --data 'action=display_results&dd1%5B%5D=All_Adjective&dd1%5B%5D=All_Noun&numberToDisplay=10' | pup 'table td strong text{}' | sort)
+while read -r name
 do
   if grep -q "$name" <<< "$PREVIOUS"
   then
