df88955dc5e3e6dddac22b294a814d39651d8e18
Rob Fletcher <rfletcher@netflix.com>
2021-07-07 08:34:22 -0700

    don't need kotlin-reflect

--- strikt-core/strikt-core.gradle.kts
+++ strikt-core/strikt-core.gradle.kts
@@ -10,7 +10,6 @@ dependencies {
   api("org.opentest4j:opentest4j:${property("versions.opentest4j")}")
 
   implementation("com.christophsturm:filepeek:${property("versions.filepeek")}")
-  implementation("org.jetbrains.kotlin:kotlin-reflect")
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 
   testImplementation("dev.minutest:minutest:${property("versions.minutest")}")
