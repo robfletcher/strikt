7f0fbb424821e5b3286ecb1b4ce082ba1706bd68
Christoph Sturm <me@christophsturm.com>
2021-12-20 21:45:49 +0100

    convert tuple assertions to failgood.

---
+++ strikt-core/src/test/kotlin/strikt/assertions/TupleAssertionsTest.kt
@@ -0,0 +1,33 @@
+package strikt.assertions
+
+import failgood.Test
+import failgood.describe
+import strikt.api.expectThat
+
+@Test
+class TupleAssertionsTest {
+  val context = describe("tuples") {
+    describe("on an expectation on a pair") {
+      val expectation = expectThat("a" to 1)
+      test("first maps to component1") {
+        expectation.first.isEqualTo("a")
+      }
+      test("second maps to component2") {
+        expectation.second.isEqualTo(1)
+      }
+    }
+    describe("on an expectation on a triple") {
+      val pair = Triple("a", "b", 1)
+      val expectation = expectThat(pair)
+      test("first maps to component1") {
+        expectation.first.isEqualTo("a")
+      }
+      test("second maps to component2") {
+        expectation.second.isEqualTo("b")
+      }
+      test("third maps to component2") {
+        expectation.third.isEqualTo(1)
+      }
+    }
+  }
+}
