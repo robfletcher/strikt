ab3a7b06a8a9cea45da7fa08c7ba713b264a2426
Rob Fletcher <rfletcher@netflix.com>
2022-01-11 08:25:38 -0800

    dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
   id("org.jmailen.kotlinter") version "3.8.0" apply false
   id("info.solidsoft.pitest") version "1.6.0" apply false
   id("com.adarshr.test-logger") version "3.1.0" apply false
-  id("com.github.ben-manes.versions") version "0.40.0"
+  id("com.github.ben-manes.versions") version "0.41.0"
   id("org.jetbrains.dokka")
   id("org.jetbrains.kotlinx.kover") version "0.4.4"
 }
@@ -119,7 +119,7 @@ configure<NexusStagingExtension> {
 
 // Dependency updates configuration
 fun ModuleComponentIdentifier.isNonStable() =
-  version.contains(Regex("""-(m|eap|rc|alpha|beta|b)(-?[\d-]+)?$""", IGNORE_CASE))
+  version.contains(Regex("""-(m|eap|rc|alpha|beta|b)([-\.]?[\d-]+)?$""", IGNORE_CASE))
 
 tasks.withType<DependencyUpdatesTask> {
   revision = "release"
