24ccb67b423a11b89ef1a3bcf0fdbbbdef657c0b
Reevn <hey@reevn.dev>
2022-01-30 00:35:57 +0100

    Add propertiesAreEqualToIgnoring assertion

--- strikt-jvm/src/main/kotlin/strikt/java/Any.kt
+++ strikt-jvm/src/main/kotlin/strikt/java/Any.kt
@@ -4,6 +4,7 @@ import strikt.api.Assertion
 import strikt.assertions.contentEquals
 import strikt.assertions.isEqualTo
 import java.beans.Introspector
+import kotlin.reflect.KProperty1
 
 /**
  * Asserts that all properties of the subject match those of [other] according
@@ -13,11 +14,32 @@ import java.beans.Introspector
  * Properties are identified using Java beans conventions.
  */
 infix fun <T : Any> Assertion.Builder<T>.propertiesAreEqualTo(other: T): Assertion.Builder<T> =
+  compareFieldByField(other)
+
+/**
+ * Asserts that all properties of the subject except [ignoredProperties] match
+ * those of [other] according to either [contentEquals] in the case of array
+ * properties or [isEqualTo] in other cases.
+ *
+ * Properties are identified using Java beans conventions.
+ */
+fun <T : Any> Assertion.Builder<T>.propertiesAreEqualToIgnoring(
+  other: T,
+  vararg ignoredProperties: KProperty1<T, Any>
+): Assertion.Builder<T> = compareFieldByField(
+  other,
+  ignoredProperties.map(KProperty1<T, Any>::name)
+)
+
+private fun <T : Any> Assertion.Builder<T>.compareFieldByField(
+  other: T,
+  ignoredPropertyNames: List<String> = emptyList()
+): Assertion.Builder<T> =
   compose("is equal field-by-field to %s", other) { subject ->
     Introspector.getBeanInfo(subject.javaClass).let { beanInfo ->
       beanInfo
         .propertyDescriptors
-        .filter { it.name != "class" }
+        .filter { it.name != "class" && it.name !in ignoredPropertyNames }
         .forEach { property ->
           val mappedAssertion = get("value of property ${property.name}") {
             property.readMethod(this)
