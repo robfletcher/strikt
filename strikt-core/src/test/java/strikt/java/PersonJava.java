32f378e0b51f0b26ad33422b54201c365f1d29ac
Rob Fletcher <rfletcher@netflix.com>
2022-02-02 16:30:40 -0800

    Remove `isNotNull` for non-nullable subjects to avoid overload ambiguity

---
+++ strikt-core/src/test/java/strikt/java/PersonJava.java
@@ -0,0 +1,11 @@
+package strikt.java;
+
+public class PersonJava {
+  private final String name;
+
+  public PersonJava(String name) {this.name = name;}
+
+  public String getName() {
+    return name;
+  }
+}
---
+++ strikt-core/src/test/kotlin/strikt/assertions/PlatformTypes.kt
@@ -0,0 +1,20 @@
+package strikt.assertions
+
+import failgood.Test
+import failgood.describe
+import strikt.api.expectThat
+import strikt.java.PersonJava
+
+@Test
+class PlatformTypes {
+  val context = describe("platform types") {
+    describe("when nullability is uncertain") {
+      val expectation = expectThat(PersonJava("Oswald Launcelot Campbell-Graves"))
+        .get(PersonJava::getName)
+      test("isNotNull can be applied") {
+        expectation.isNotNull()
+      }
+    }
+  }
+
+}
