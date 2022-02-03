ec8f05e4bb25f407d35b1b27b81eb650b6754e79
Rob Fletcher <rfletcher@netflix.com>
2022-02-02 16:35:58 -0800

    bump Kover plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
   id("com.adarshr.test-logger") version "3.1.0" apply false
   id("com.github.ben-manes.versions") version "0.41.0"
   id("org.jetbrains.dokka")
-  id("org.jetbrains.kotlinx.kover") version "0.4.4"
+  id("org.jetbrains.kotlinx.kover") version "0.5.0"
 }
 
 repositories {
