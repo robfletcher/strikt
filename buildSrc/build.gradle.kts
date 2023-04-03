262a7ee08e51916733a3d84d1fe8314e6c153a32
Rob Fletcher <rfletcher@netflix.com>
2023-04-03 12:23:31 -0700

    update dokka

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,6 +10,6 @@ repositories {
 }
 
 dependencies {
-  implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.7.20")
+  implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.8.10")
   implementation("com.netflix.nebula:nebula-publishing-plugin:20.2.0")
 }
