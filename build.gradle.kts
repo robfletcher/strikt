befe8c42d74ed830a2ee0dbe7134cf586452c920
Rob Fletcher <rfletcher@netflix.com>
2023-04-03 11:30:48 -0700

    dependency updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,9 @@ import kotlin.text.RegexOption.IGNORE_CASE
 plugins {
   kotlin("jvm") apply false
   id("io.codearte.nexus-staging") version "0.30.0"
-  id("org.jmailen.kotlinter") version "3.13.0" apply false
+  id("org.jmailen.kotlinter") version "3.14.0" apply false
   id("com.adarshr.test-logger") version "3.2.0" apply false
-  id("com.github.ben-manes.versions") version "0.45.0"
+  id("com.github.ben-manes.versions") version "0.46.0"
   id("org.jetbrains.dokka")
   id("org.jetbrains.kotlinx.kover") version "0.6.1"
 }
